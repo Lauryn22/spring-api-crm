5bf83f5235e9b0970a45693496e2c60573b0b9d9
laurentbila <bin_bila@yahoo.fr>
2022-08-31 16:04:42 +0200

    first commit

---
+++ src/test/java/fr/m2i/springapicrm/SpringapicrmApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.m2i.springapicrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapicrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
